5c38f548b694143ad1ad8ce5529bb08e8e80668b
Gustavo <gusta.coliveira07@gmail.com>
2021-01-15 08:45:38 -0300

    training promises

--- asynchronous/promises.js
+++ asynchronous/promises.js
@@ -6,39 +6,67 @@ function random(min, max){
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function connectionDB(msg, time){
+// function connectionDB(msg, time){
+//     return new Promise((resolve, reject)=>{
+//         if (typeof msg !== 'string'){
+//             const err = 'Erro 404';
+//             reject(err);
+//         }
+//         setTimeout(()=>{
+//             resolve(msg);
+//         }, time)
+//     })
+// }
+
+
+// connectionDB('Connection database', random(1, 3))
+//     .then(response =>{
+//         console.log(response);
+//         return connectionDB('Searching in database', random(1, 3))
+//     })
+//     .then(response =>{
+//         console.log(response);
+//         return connectionDB('handling the data', random(1, 3));
+//     })
+//     .then(response =>{
+//         console.log(response);
+//         return connectionDB('displaying data on the screen', random(', 3'))
+//     })
+//     .then(response =>{
+//         console.log(response);
+//     })
+//     .catch(err =>{
+//         console.log(err);
+//     });
+
+    
+console.log('Isso irá executar antes de tudo!');
+//Código treino promises
+
+function Search(msg){
     return new Promise((resolve, reject)=>{
-        if (typeof msg !== 'string'){
-            const err = 'Erro 404';
-            reject(err);
+        const database = [
+            'learn javascript', 'learn python', 
+            'learn C#', 'self studies', 'Curso em vídeo',
+            'excel', 'pascal', 'visual basic'
+        ]
+        const data = database.find(d => d === msg );
+
+        if(data !== undefined){
+            setTimeout(()=>{
+                resolve(`Search: ${msg}`);
+            }, random(1, 5));
+        }else{
+            reject('Error 404 [Not Found]');
         }
-        setTimeout(()=>{
-            resolve(msg);
-        }, time)
     })
 }
 
 
-connectionDB('Connection database', random(1, 3))
-    .then(response =>{
-        console.log(response);
-        return connectionDB('Searching in database', random(1, 3))
-    })
-    .then(response =>{
-        console.log(response);
-        return connectionDB('handling the data', random(1, 3));
-    })
-    .then(response =>{
-        console.log(response);
-        return connectionDB('displaying data on the screen', random(', 3'))
-    })
+Search('java')
     .then(response =>{
         console.log(response);
     })
     .catch(err =>{
         console.log(err);
-    });
-
-    
-console.log('Isso irá executar antes de tudo!');
-//Código treino promises
+    })
