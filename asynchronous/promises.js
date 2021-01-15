de9e9fce05b8d99743e52e26afc26a842440ec0c
Gustavo <gusta.coliveira07@gmail.com>
2021-01-15 09:12:51 -0300

    learning promises

--- asynchronous/promises.js
+++ asynchronous/promises.js
@@ -43,18 +43,31 @@ function random(min, max){
 //console.log('Isso irá executar antes de tudo!');
 //Código treino promises
 
+function Browser(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve('Open browser');
+        }, random(1, 5));
+    })
+}
+
 function Search(msg){
     return new Promise((resolve, reject)=>{
         const database = [
-            'learn javascript', 'learn python', 
-            'learn C#', 'self studies', 'Curso em vídeo',
-            'excel', 'pascal', 'visual basic'
+            {search: 'learn javascript', results: 14}, 
+            {search: 'learn python', results: 15},
+            {search: 'learn C#', results: 10},
+            {search: 'self studies', results: 10},
+            {search: 'Curso em vídeo', results: 17},
+            {search: 'excel', results: 6},
+            {search: 'pascal', results: 9},
+            {search: 'visual basic', results: 7},
         ]
-        const data = database.find(d => d === msg );
+        const data = database.find(d => d.search === msg );
 
         if(data !== undefined){
             setTimeout(()=>{
-                resolve(`Search: ${msg}`);
+                resolve(`Search: ${data.search} \nResults: ${data.results}`);
             }, random(1, 5));
         }else{
             reject('Error 404 [Not Found]');
@@ -62,8 +75,11 @@ function Search(msg){
     })
 }
 
-
-Search('java')
+Browser()
+    .then(resolve =>{
+        console.log(resolve);
+        return Search('learn javascript');
+    })
     .then(response =>{
         console.log(response);
     })
