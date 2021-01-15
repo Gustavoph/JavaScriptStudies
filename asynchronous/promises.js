1ad648e165be8338ea139f19bf3415db1a9c50f2
Gustavo <gusta.coliveira07@gmail.com>
2021-01-15 09:13:40 -0300

    learning promises

--- asynchronous/promises.js
+++ asynchronous/promises.js
@@ -6,41 +6,41 @@ function random(min, max){
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-// function connectionDB(msg, time){
-//     return new Promise((resolve, reject)=>{
-//         if (typeof msg !== 'string'){
-//             const err = 'Erro 404';
-//             reject(err);
-//         }
-//         setTimeout(()=>{
-//             resolve(msg);
-//         }, time)
-//     })
-// }
+function connectionDB(msg, time){
+    return new Promise((resolve, reject)=>{
+        if (typeof msg !== 'string'){
+            const err = 'Erro 404';
+            reject(err);
+        }
+        setTimeout(()=>{
+            resolve(msg);
+        }, time)
+    })
+}
 
 
-// connectionDB('Connection database', random(1, 3))
-//     .then(response =>{
-//         console.log(response);
-//         return connectionDB('Searching in database', random(1, 3))
-//     })
-//     .then(response =>{
-//         console.log(response);
-//         return connectionDB('handling the data', random(1, 3));
-//     })
-//     .then(response =>{
-//         console.log(response);
-//         return connectionDB('displaying data on the screen', random(', 3'))
-//     })
-//     .then(response =>{
-//         console.log(response);
-//     })
-//     .catch(err =>{
-//         console.log(err);
-//     });
+connectionDB('Connection database', random(1, 3))
+    .then(response =>{
+        console.log(response);
+        return connectionDB('Searching in database', random(1, 3))
+    })
+    .then(response =>{
+        console.log(response);
+        return connectionDB('handling the data', random(1, 3));
+    })
+    .then(response =>{
+        console.log(response);
+        return connectionDB('displaying data on the screen', random(', 3'))
+    })
+    .then(response =>{
+        console.log(response);
+    })
+    .catch(err =>{
+        console.log(err);
+    });
 
     
-//console.log('Isso irá executar antes de tudo!');
+console.log('Isso irá executar antes de tudo!');
 //Código treino promises
 
 function Browser(){
